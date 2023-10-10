115ff45cade80900c9a43bbc97183bfa1f49ec4c
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-10 17:32:40 +1100

    Update menu bar
    Update so the menu will show and disappear when the user toggle it. Making a very appealing design.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
+// Menu show
+// validate if content exists
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
+// menu hidden
+// validate if content exists
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
